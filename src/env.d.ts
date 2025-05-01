1bc841a94fc674b2078601d1af0719a7e8260d9c
Hansky Mulya <me@hansky.dev>
2025-05-01 19:26:40 +0700

    feat: add theme support

---
+++ src/env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="astro/client" />
+
+interface Window {
+  theme: {
+    setTheme: (theme: "system" | "dark" | "light") => void;
+    getTheme: () => "system" | "dark" | "light";
+    getSystemTheme: () => "light" | "dark";
+    getDefaultTheme: () => "system" | "dark" | "light";
+  };
+}
