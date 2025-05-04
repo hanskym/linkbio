0b259b541eabacee9eefead3e483393fe73dd775
Hansky Mulya <me@hansky.dev>
2025-05-04 22:30:35 +0700

    fix: code adjustment

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
   adapter: cloudflare(),
   prefetch: true,
   trailingSlash: 'never',
+  build: {
+    format: 'file',
+  },
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
