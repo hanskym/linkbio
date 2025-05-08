91d8d04f2655a71e72738620fba17536c5b4ec27
Hansky Mulya <me@hansky.dev>
2025-05-08 11:00:00 +0700

    fix: update siteConfig URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://linkbio.hansky.dev',
+  site: 'https://linkbio-hsky.pages.dev',
   output: 'static',
   adapter: cloudflare(),
   prefetch: true,
